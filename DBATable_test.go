276338d909e814c96b2cd005f28268178181cf9a
clakeboy <clakeboy@gmail.com>
2020-12-07 13:54:25 +0800

    update test

--- DBATable_test.go
+++ DBATable_test.go
@@ -1,7 +1,12 @@
 package ck_orm
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestDBATable_Find(t *testing.T) {
-
+	fmt.Println("BEGIN...")
+	fmt.Println("test print")
+	fmt.Println("End ...")
 }
